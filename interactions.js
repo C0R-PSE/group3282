1312ae5a1aa2e58d908482a070b41939dc878336
CORPSE <d.b.17f@mail.ru>
2024-11-22 22:39:10 +0300

    1

--- interactions.js
+++ interactions.js
@@ -2,9 +2,11 @@ console.log(window.Telegram)
 window.Telegram.WebApp.requestFullscreen()
 function buttonPress(button) {
     button.classList.toggle("active")
+    testButton.innerHTML = window.Telegram.WebApp.contentSafeAreaInset.top + "<br>" + window.Telegram.WebApp.safeAreaInset.top
 }
 var grid = document.querySelector(".grid")
 var gridElements = grid.getElementsByTagName('div')
+var testButton = grid.querySelector(".test")
 for (let i=0; i < gridElements.length; i++) {
     gridElements[i].addEventListener("click", () => buttonPress(gridElements[i]))
 }
