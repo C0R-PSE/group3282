e56d87cb992c3e6abd4a7725a9492266bf627532
CORPSE <d.b.17f@mail.ru>
2024-11-22 21:17:01 +0300

    1

--- interactions.js
+++ interactions.js
@@ -1,10 +1,10 @@
+window.Telegram.WebApp.requestFullscreen()
+console.log(window.Telegram)
 function buttonPress(button) {
     button.classList.toggle("active")
 }
 var grid = document.querySelector(".grid")
 var gridElements = grid.getElementsByTagName('div')
-console.log(gridElements)
 for (let i=0; i < gridElements.length; i++) {
-    console.log(gridElements[i])
     gridElements[i].addEventListener("click", () => buttonPress(gridElements[i]))
 }
