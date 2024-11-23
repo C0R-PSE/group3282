e0d056ec77f7946dc503256cc513d33725dd19d7
CORPSE <d.b.17f@mail.ru>
2024-11-23 17:06:47 +0300

    1

--- interactions.js
+++ interactions.js
@@ -10,3 +10,17 @@ var gridElements = grid.getElementsByTagName('div')
 for (let i=0; i < gridElements.length; i++) {
     gridElements[i].addEventListener("click", () => buttonPress(gridElements[i]))
 }
+
+/*await fetch('https://api.telegram.org/bot7976528964:AAEFqVm3gOS2E6GUZfOB9mKv_kMijlhikDM/sendMessage?chat_id=1307263371&text=' + "test1").then(resp => resp.json())*/
+fetch("https://evstakhii.d-b-17f.workers.dev/", {
+    method: "POST",
+    body: JSON.stringify({
+      userId: 1,
+      title: "Fix my bugs",
+      completed: false
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then(resp => resp.json())
+/*console.log(encodeURI(JSON.stringify(window.Telegram)))*/
