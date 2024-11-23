9faa891ced80c03e97735a7ea45f3b4292777f38
CORPSE <d.b.17f@mail.ru>
2024-11-23 21:53:40 +0300

    123

--- interactions.js
+++ interactions.js
@@ -1,15 +1,24 @@
+const botKey = "7976528964:AAEFqVm3gOS2E6GUZfOB9mKv_kMijlhikDM"
+const channelId = "-1002270219468"
+
+const openButton = '{"inline_keyboard":[[{"text":"Открыть Журнал","url":"https://t.me/mytestbot2211bot?startapp"}]]}'
+
 async function buttonPress(button) {
     button.classList.toggle("active")
     var pos = timestamps.indexOf(button.innerText)
     checks[pos] = 1 - checks[pos]
     
 }
-async function confirmPress() {await fetch('https://api.telegram.org/bot7976528964:AAEFqVm3gOS2E6GUZfOB9mKv_kMijlhikDM/sendMessage?chat_id=-1002270219468&text=' + 
-    JSON.stringify({
-        checks,
-        timestamps
-    })
-)}
+async function confirmPress() { 
+    await sendRequest("sendMessage", {
+        "chatId" : channelId,
+        "text" : JSON.stringify({
+            checks,
+            timestamps
+        })
+})}
+
+var JournalMessage = sendRequest("getChat", {"chat_id" : channelId})
 
 console.log(window.Telegram)
 /*window.Telegram.WebApp.requestFullscreen()*/
@@ -34,3 +43,17 @@ confirmButton.addEventListener('click', () =>{confirmPress()})
 grid.append(confirmButton)
 
 /*console.log(encodeURI(JSON.stringify(window.Telegram)))*/
+'https://api.telegram.org/bot7976528964:AAEFqVm3gOS2E6GUZfOB9mKv_kMijlhikDM/sendMessage?chat_id=-1002270219468&text=123&reply_markup='
+
+async function sendRequest(botMethod, options) {
+    var url = 'https://api.telegram.org/bot' + botKey + '/' + botMethod + '?'
+    for (var i in options) {
+        url += "&" + i + "="
+        if (typeof(options[i]) == "object") {
+            url += JSON.stringify(options[i])
+        } else {
+            url += options[i]
+        }
+    }
+    await fetch(encodeURI(url)).then(resp => resp.json())
+}
