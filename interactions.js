9eadb81893f9c139ea8d332b4d66d8489c9e8c1d
CORPSE <d.b.17f@mail.ru>
2024-11-23 17:37:35 +0300

    1

--- interactions.js
+++ interactions.js
@@ -12,15 +12,8 @@ for (let i=0; i < gridElements.length; i++) {
 }
 
 /*await fetch('https://api.telegram.org/bot7976528964:AAEFqVm3gOS2E6GUZfOB9mKv_kMijlhikDM/sendMessage?chat_id=1307263371&text=' + "test1").then(resp => resp.json())*/
-fetch("https://evstakhii.d-b-17f.workers.dev/", {
+await fetch("https://evstakhii.d-b-17f.workers.dev/", {
     method: "POST",
-    body: JSON.stringify({
-      userId: 1,
-      title: "Fix my bugs",
-      completed: false
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then(resp => resp.json())
+    body: "123"
+    }).then(resp => resp.json())
 /*console.log(encodeURI(JSON.stringify(window.Telegram)))*/
