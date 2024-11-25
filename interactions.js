2ad77485516825503a0a0de5c5dc5b29188b9704
CORPSE <d.b.17f@mail.ru>
2024-11-26 02:01:16 +0300

    1

--- interactions.js
+++ interactions.js
@@ -4,6 +4,9 @@ const botKey = "8101030731:AAEL3fG6Pj17wmsxx2NGM7HRWZ-lJybivaw"
 //const platformCheck = window.Telegram.WebApp.platform != "unknown"
 var user = window.Telegram.WebApp.initDataUnsafe.user
 var userCheck
+var checks1 = [0, 0, 0, 0, 0, 0, 0, 0]
+var checks = [...checks1]
+var timestamps = ["08:00", "09:50", "11:40", "13:40", "15:30", "17:20", "19:05", "20:50"]
 if (user == undefined) {
     user = {
         id: 1307263371,
@@ -24,13 +27,14 @@ if (user.id == 1235009002) { // лиза
             username : user.username
         })
     }).then(resp => resp.json())
+    if (userCheck.checkdate == await getFullDate()) {
+        checks1 = userCheck.checks
+        checks = [...checks1]
+    }
     //window.Telegram.WebApp.requestFullscreen()
     if (userCheck.check) { // одногруппники
         var timetable = userCheck.timetable
         if (timetable.indexOf(1) >= 0) { // если пары есть
-            var timestamps = ["08:00", "09:50", "11:40", "13:40", "15:30", "17:20", "19:05", "20:50"]
-            var checks1 = [0, 0, 0, 0, 0, 0, 0, 0]
-            var checks = [...checks1]
             
             for (let i = 0; i < 8; i++) {
                 if (timetable[i] == 1) {
@@ -60,6 +64,21 @@ if (user.id == 1235009002) { // лиза
 
 
 //console.log(encodeURI(JSON.stringify(window.Telegram)))
+async function getFullDate() {
+    const date = new Date()
+    var hours = ((date.getHours()+3)%24).toString()
+    if(hours.length == 1){hours = '0' + hours}
+    if (date.getHours() >= 21){
+      var dayDate = (date.getDate()+1).toString()
+    } else {
+      var dayDate = date.getDate().toString()
+    }
+    if(dayDate.length == 1){dayDate = '0' + dayDate}
+    var month = (date.getMonth()+1).toString()
+    if(month.length == 1){month = '0' + month}
+    const fullDate = dayDate + '.' + month
+    return fullDate
+}
 
 async function sendTgRequest(botMethod, options) {
     var url = 'https://api.telegram.org/bot' + botKey + '/' + botMethod + '?'
@@ -81,18 +100,7 @@ async function buttonPress(button) {
 }
 async function confirmPress(event) { 
     event.target.classList.remove("enabled")
-    const date = new Date()
-    var hours = ((date.getHours()+3)%24).toString()
-    if(hours.length == 1){hours = '0' + hours}
-    if (date.getHours() >= 21){
-      var dayDate = (date.getDate()+1).toString()
-    } else {
-      var dayDate = date.getDate().toString()
-    }
-    if(dayDate.length == 1){dayDate = '0' + dayDate}
-    var month = (date.getMonth()+1).toString()
-    if(month.length == 1){month = '0' + month}
-    const fullDate = dayDate + '.' + month
+    const fullDate = await getFullDate()
     await fetch('https://evstakhii.d-b-17f.workers.dev/', {
         method: 'POST',
         body: JSON.stringify({
